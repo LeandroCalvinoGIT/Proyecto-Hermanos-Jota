93df9037d01a759b48e476e013bbc9ae6257f569
LeandroCalvinoGITutn <lecalvino@frba.utn.ar>
2025-09-04 09:01:51 -0300

    #1.1 Archivos Base y Enunciados

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Proyecto-Hermanos-Jota
+# Proyecto-Hermanos-Jota 
+
+#Integrantes:
+- Agustina Ailen Resquin
+- Leandro Calviño
+- Noelia Agustina Brandan
+- Gonzalo Quintana
+- Franco Baranovich
+
+#Funcionalidades Implementadas
+
+#Tecnologías Utilizadas
---
+++ code/main.js
@@ -0,0 +1,5 @@
+/* JavaScript:
+- Prohibido "hardcodear" los productos en el HTML. Los datos deben estar en un array de objetos en un archivo .js.  
+- El catálogo de productos debe renderizarse dinámicamente manipulando el DOM.
+- Se debe simular una carga asíncrona para los productos (setTimeout con Promises o async await).
+- Toda la interactividad debe manejarse con addEventListener. */
